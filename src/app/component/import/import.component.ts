43629e0ae90791a3babc14707a756c2eb9f9b54a
tokoroi <basket1936022@gmail.com>
2023-08-07 08:50:01 +0900

    読み込んだExcelの中身を表示するように変更

--- src/app/component/import/import.component.ts
+++ src/app/component/import/import.component.ts
@@ -7,22 +7,36 @@ import * as XLSX from 'xlsx';
 })
 export class ImportComponent {
 
+  // 読み込んだデータを格納する配列
+  data: any[] = [];
+  // ヘッダー(1行目)
+  headers: string[] = [];
+
  // ファイルが選択された時のイベントハンドラ
 onFileChange(event: any) {
   const file = event.target.files[0];
-  const fileReader = new FileReader();
-  
-  fileReader.onload = (e: any) => {
-    const arrayBuffer = e.target.result;
-    const workbook = XLSX.read(arrayBuffer, { type: 'array' });
-    const firstSheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[firstSheetName];
-    
-    // データの取得などの処理を行う
-    const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-    console.log(data);
-  };
-  
-  fileReader.readAsArrayBuffer(file);
+  this.readExcel(file);
 }
+  // Excelファイル読込処理
+  readExcel(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const data = new Uint8Array(e.target.result);
+      // Excelファイル読込
+      const workbook = XLSX.read(data, { type: 'array' });
+      // シート名を取得
+      const sheetName = workbook.SheetNames[0];
+      // 取得したシート名の中身を取得
+      const worksheet = workbook.Sheets[sheetName];
+      // json形式に変換
+      this.data = XLSX.utils.sheet_to_json(worksheet, {header: 1 });
+      // ヘッダー行の取得
+      this.headers = this.data[0];
+      // 中身からはヘッダー行を削除する
+      this.data.shift();
+    };
+    // ファイルの内容を一時的にメモリに保存する(バッファーとして読み込む)
+    reader.readAsArrayBuffer(file);
+  }
+
 }
