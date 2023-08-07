df504010c358192fc72302f3527a2457a4ad915c
市原紀葉 <ichihara_kazuha@cre-nagoya.com>
2023-08-07 14:16:58 +0900

    addtask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,15 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { DatabaseComponent } from './component/database/database.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskComponent } from './task/task.component';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,13 +25,22 @@ import { DatabaseComponent } from './component/database/database.component';
     ImportComponent,
     HomeComponent,
     NotFoundComponent,
-    DatabaseComponent
+    DatabaseComponent,
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule
 
   ],
   providers: [],
