9a476875b107f1299675679b95bf2e077a52346d
市原紀葉 <ichihara_kazuha@cre-nagoya.com>
2023-08-08 10:32:59 +0900

    delete

--- src/app/component/database/database.component.ts
+++ src/app/component/database/database.component.ts
@@ -24,8 +24,27 @@ todo: Task[] = [
 inProgress: Task[] = [];
 done: Task[] = [];
 
-editTask(list: string, task:Task): void {}
- 
+editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+  const dialogRef = this.dialog.open(TaskDialogComponent, {
+    width: '270px',
+    data: {
+      task,
+      enableDelete: true,
+    },
+  });
+  dialogRef.afterClosed().subscribe((result: TaskDialogResult|undefined) => {
+    if (!result) {
+      return;
+    }
+    const dataList = this[list];
+    const taskIndex = dataList.indexOf(task);
+    if (result.delete) {
+      dataList.splice(taskIndex, 1);
+    } else {
+      dataList[taskIndex] = task;
+    }
+  });
+}
 drop(event: CdkDragDrop<Task[]>): void {
   if (event.previousContainer === event.container) {
     return;
